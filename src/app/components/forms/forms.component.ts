5208a6db81a047706c29add2b37ab51bc357a52b
PedroFanan <fananpedro@hotmail.com>
2024-01-03 22:20:12 -0300

    creating the forms component and styling

---
+++ src/app/components/forms/forms.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forms',
+  standalone: true,
+  imports: [],
+  templateUrl: './forms.component.html',
+  styleUrl: './forms.component.scss'
+})
+export class FormsComponent {
+
+results =[];
+
+constructor() { }
+
+ngOnInit(): void {
+
+}
+
+}
